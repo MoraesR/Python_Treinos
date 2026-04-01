d724af1d9c03b0d60dbcdbae9e2754448bf71a19
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-03-31 23:39:49 -0400

    Primeiro Commit atividade Cadastro de Restaurantes

---
+++ atividade1.py
@@ -0,0 +1,18 @@
+print(' 1. Cadastrar Restaurante')
+print(' 2. Listar Restaurantes')
+print(' 3. Ativar Restaurante')
+print(' 4. Sair')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar Restaurante1')
+elif opcao_escolhida == 4:
+    print('Finalizar App')
+else:
+    print('Opção inválida!')
+    
