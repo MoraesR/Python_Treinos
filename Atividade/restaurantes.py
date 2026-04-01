9d9dccbba4662b480366bbc945383b433cce3e14
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-04-01 00:44:59 -0400

    Atualizando codigo do cadastro de restaurante

--- Atividade/restaurantes.py
+++ Atividade/restaurantes.py
@@ -1,18 +1,74 @@
-print(' 1. Cadastrar Restaurante')
-print(' 2. Listar Restaurantes')
-print(' 3. Ativar Restaurante')
-print(' 4. Sair')
+import os
+restaurantes = []
+def exibir_nome_do_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+def exibir_opcoes():
+    print(' 1. Cadastrar Restaurante')
+    print(' 2. Listar Restaurantes')
+    print(' 3. Ativar Restaurante')
+    print(' 4. Sair')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
+def escolher_opcao():
 
-if opcao_escolhida == 1:
-    print('Cadastrar Restaurante')
-elif opcao_escolhida == 2:
-    print('Listar Restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar Restaurante1')
-elif opcao_escolhida == 4:
-    print('Finalizar App')
-else:
-    print('Opção inválida!')
     
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante1')
+    elif opcao_escolhida == 4:
+        print('Finalizar App')
+    else:
+        print('Opção inválida!')
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu ')
+    main()
+
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
+
+
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def finalizar_app():
+    exibir_subtitulo('Finalizar app')
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()  
