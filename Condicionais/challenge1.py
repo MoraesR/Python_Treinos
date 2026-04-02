d0fa4e3e199a3c5bfd7fa26917e5c98159994e30
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-04-02 15:15:51 -0400

    challenge de condicionais - 1

---
+++ Condicionais/challenge1.py
@@ -0,0 +1,17 @@
+print("""                                               
+,--.   ,--.                ,--.                
+ \  `.'  /,---. ,--,--,  ,-|  | ,--,--. ,---.  
+  \     /| .-. :|      \' .-. |' ,-.  |(  .-'  
+   \   / \   --.|  ||  |\ `-' |\ '-'  |.-'  `) 
+    `-'   `----'`--''--' `---'  `--`--'`----'  
+""")
+
+p1 = int(input('Digite o número de venda do abacaxi: '))
+p2 = int(input('Digite o número de venda da banana:  '))
+
+if p1 > p2:
+    print(f'O número de vendas do Abacaxi é maior que o de Banana: {p1} ')
+elif p2 > p1:
+    print(f'O número de vendas da Banana é maior que o de Abacaxi: {p2} ')
+else:
+    print("empate")
