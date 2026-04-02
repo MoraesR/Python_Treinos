9bc322918ac3ebe0f62cee16693fa6ce92e706bc
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-04-02 15:25:56 -0400

    atulizando challenge 1

--- Condicionais/challenge1.py
+++ Condicionais/challenge1.py
@@ -9,6 +9,9 @@ print("""
 p1 = int(input('Digite o número de venda do abacaxi: '))
 p2 = int(input('Digite o número de venda da banana:  '))
 
+print(f'Vendas do Abacaxi: {p1}')
+print(f'Vendas do Abacaxi: {p2}')
+
 if p1 > p2:
     print(f'O número de vendas do Abacaxi é maior que o de Banana: {p1} ')
 elif p2 > p1:
