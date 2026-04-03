5fccf98ace5300c6ab569b8245116aa845fa9dbb
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-04-02 23:25:53 -0400

    challenge2

---
+++ Condicionais/challenge2.py
@@ -0,0 +1,6 @@
+#
+
+a = int(input('Informe o número de dias para a atividade A: '))
+b = int(input('Informe o número de dias para a atividade B: '))
+c = int(input('Informe o número de dias para a atividade C: '))
+
