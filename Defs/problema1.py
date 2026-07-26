eeeec7df427540aca79b9596685085643100d92e
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-07-26 00:01:58 -0400

    ContaBanc

---
+++ Defs/problema1.py
@@ -0,0 +1,19 @@
+class ContaBanc():
+    def __init__(self, titulo, saldo):
+        self.titulo = titulo
+        self.saldo = saldo
+        
+    def depositar(self, valor):
+        self.saldo = self.saldo + valor
+        print(f"Depósito de R${valor} realizado")
+
+
+minhaconta = ContaBanc("Rodrigo", 1000.0)
+minhaconta.depositar(100)
+
+
+outraconta = ContaBanc("Maria", 2000.0)
+outraconta.depositar(200)
+
+print(minhaconta.saldo)
+print(outraconta.saldo)
